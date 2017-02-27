9760799f5418b36f53c144fe2bc68b86089384c4
ross <ross@THEWALKINGDEAD>
2017-02-26 21:56:21 -0600

    https://github.com/Rossamino/OOP-Basic-to-Advanced.git

---
+++ FirstSimpleConsole/OOP1/Person.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1.Polymorphism {
+
+    class Person {
+
+        #region Data or Fields without Encapsulation
+
+        public string FirstName;
+        public string LastName;
+        public DateTime DateOfBirth;
+        /// <summary>
+        /// False : Female
+        /// </summary>
+        public bool Gender;
+
+        #endregion
+
+        #region Logic or Metods or Functions or Behaviors
+
+        public void Walk() {
+            Console.WriteLine(FirstName + " is walking.");
+        }
+
+        public void Eat() {
+            Console.WriteLine(FirstName + " is eating.");
+        }
+
+        #endregion
+
+        #region Destructor
+        ~Person() {
+            //destructor
+            Console.WriteLine("Destructor is called.");
+        }
+        #endregion
+    }
+}
---
+++ FirstSimpleConsole/OOP1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using OOP1.Polymorphism;
+
+namespace OOP1
+{
+    class Program
+    {
+        public static void Main(string[] args) {
+            Person person = new Person();
+            person.FirstName = "Alice";
+            person.Walk();
+
+            var destructor = new DestructorClass();
+            destructor = null;
+            GC.Collect();
+
+            Console.ReadKey();
+        }
+    }
+}
+
